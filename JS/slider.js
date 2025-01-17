05eba1aa185d405e57b0c5810596388d08c6d932
Abhiii08 <abhibadhe2002@gmail.com>
2025-01-17 18:03:04 +0530

    commit

--- JS/slider.js
+++ JS/slider.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const sections = document.querySelectorAll('.project-container, .quality-container'); // Select all relevant sections
+    const sections = document.querySelectorAll('.project-container, .team-container'); // Select all relevant sections
 
     sections.forEach(section => {
         const projectGrid = section.querySelector('.slide'); // Get the corresponding slide container
         const leftButton = section.querySelector('.slider-button.left');
         const rightButton = section.querySelector('.slider-button.right');
-        const scrollAmount = 400 // Adjust the scroll amount per click
+        const scrollAmount = 600 // Adjust the scroll amount per click
 
         if (leftButton && rightButton && projectGrid) {
             leftButton.addEventListener('click', () => {
